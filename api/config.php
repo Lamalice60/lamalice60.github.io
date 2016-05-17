41a57816bf80a47689ee415b72ced3d962d234fe
Johan PIATKOWSKI <lamalice60@gmail.com>
2016-05-17 17:21:14 +0200

    addingDefaultHTML

---
+++ api/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', '14XiEso9DKiEkhdejwvfLA');
+    define('CONSUMER_SECRET', 'K1IcprfP7ZtHW9SM9pag9YWJ0jUWi6vAWZ3hX2FvspI');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '234626957-zSPIRl4nvdXvzffaXjZUZNVFxleenIQjOS2B749p');
+    define('ACCESS_SECRET', 'PAiDJWzd1nqVm2pzMnnFeY5OW1OiCieXSqkivO6PUM');
+	
+	// Cache Settings
+	define('CACHE_ENABLED', false);
+	define('CACHE_LIFETIME', 3600); // in seconds
+	define('HASH_SALT', md5(dirname(__FILE__)));
